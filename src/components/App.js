037aa15ac2e6af8401b3876aae28b9725254bb3a
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-10-29 21:35:18 +0530

    Refactor App component to use useState for weather

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [weather] = useState({
+    temperature: 25,
+    conditions: "Sunny",
+  });
+  const tempStyle = {
+    color: weather.temperature > 20 ? "red" : "blue",
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>
+        Temperature: <span style={tempStyle}>{weather.temperature}°C</span>
+      </p>
+      <p>Conditions: {weather.conditions}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
