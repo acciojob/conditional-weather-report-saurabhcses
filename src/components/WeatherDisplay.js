140c93a311be795b78d50d4c68e01ee9f84070ff
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-10-29 21:30:10 +0530

    Create WeatherDisplay component in App

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
+
+const App = () => {
+  const [weather] = useState({
+    temperature: 25,
+    conditions: "Sunny",
+  });
+
+  return (
+    <div>
+      <WeatherDisplay weather={weather} />
+    </div>
+  );
+}
+
+export default App;
